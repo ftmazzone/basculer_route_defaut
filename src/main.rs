70afc6f4b63dbe18a755715b7b8fc3a3292fe86d
Florent Mazzone <ftmazzone@yahoo.fr>
2021-05-24 15:59:21 +0200

    Mettre en forme

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,16 @@ fn main() {
         let mut routes = gestionnaire_de_routes::lister_routes();
 
         for (interface, _route) in &mut routes {
-             gestionnaire_de_routes::tester_route(interface, &mut interfaces);
-   
+            gestionnaire_de_routes::tester_route(interface, &mut interfaces);
         }
         gestionnaire_de_routes::calculer_duree_moyenne(&mut interfaces);
-     let routes_triees=   gestionnaire_de_routes::trier_routes(INTERFACE_PRIVILEGIEE,  routes, &mut interfaces);
-  
-        for  route in  routes_triees {
+        let routes_triees =
+            gestionnaire_de_routes::trier_routes(INTERFACE_PRIVILEGIEE, routes, &mut interfaces);
+
+        for route in routes_triees {
             println!(
                 "Interface : '{}' Métrique : {:?} Note : {:?} Route : '{}'",
-                route.interface,
-                route.metrique,
-                route.note,
-                route.route
+                route.interface, route.metrique, route.note, route.route
             );
         }
 
